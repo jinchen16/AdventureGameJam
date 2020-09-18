c06a6597af3fcd66586a9a1e65ec7e97e452d4ab
Jin Chen <ac.chencho@gmail.com>
2020-09-17 22:36:17 -0400

    Setting up Unreal Project for Jam

---
+++ AdventureJam_SRC/Source/AdventureJam_SRC/AdventureJam_SRCGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AdventureJam_SRCGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ADVENTUREJAM_SRC_API AAdventureJam_SRCGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
